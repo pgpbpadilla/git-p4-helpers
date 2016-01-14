a459ce535a0ded82cc4776613e4221175c844375
Pablo Padilla <pgpb.padilla@gmail.com>
2016-01-14 11:50:36 -0800

    new: usr: New script to create the links to shell scripts

---
+++ create_links.sh
@@ -0,0 +1,19 @@
+function create_links {
+
+    if [ "$#" -ne 1 ]; then
+	echo -ev "\nIllegal number of parameters\n"
+	echo -e "\nUsage: \$ $0 <target-path>\n"
+	return -1
+    fi
+    
+    local TGT_DIR=$1
+
+    ln -s patch_for_p4.sh $TGT_DIR/p4-patch
+    ln -s p4_reconcile_files.sh $TGT_DIR/p4-recon
+    ln -s p4-desc.sh $TGT_DIR/p4-desc
+    ln -s update-last-green.sh $TGT_DIR/green-up
+
+    return 0
+}
+
+create_links "$@"
