e2c18f5eafbd8574f3fdaee0bb53b8a0b04b802c
jessebrown@expedia.com <jessebrown@expedia.com>
2016-02-16 15:01:57 -0800

    Remove symbolic from ln arguments

--- create_links.sh
+++ create_links.sh
@@ -8,10 +8,10 @@ function create_links {
     
     local TGT_DIR=$1
 
-    ln -s patch_for_p4.sh $TGT_DIR/p4-patch
-    ln -s p4_reconcile_files.sh $TGT_DIR/p4-recon
-    ln -s p4-desc.sh $TGT_DIR/p4-desc
-    ln -s update-last-green.sh $TGT_DIR/green-up
+    ln patch_for_p4.sh $TGT_DIR/p4-patch
+    ln p4_reconcile_files.sh $TGT_DIR/p4-recon
+    ln p4-desc.sh $TGT_DIR/p4-desc
+    ln update-last-green.sh $TGT_DIR/green-up
 
     return 0
 }
